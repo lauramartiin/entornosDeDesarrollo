13b1db8dea4115bd58d43494049b17eafc0210e9
DAM1 <DAM1@PC>
2025-02-19 08:57:04 +0100

    19/02/2025 - v.0.0.0 - Nuevo cambio subido - LMartín

--- GitHubEntornos/src/HolaMundo.java
+++ GitHubEntornos/src/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola, mundo");
 		System.out.println("cambio");
-
+		System.out.println("Con permiso buenas tardes");
 	}
 
 }
